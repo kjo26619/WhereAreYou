85168dae9b2b8c954fb42dce988aba24f32a1c72
hhaa986 <hhaa9876@naver.com>
2022-05-24 21:17:16 +0900

    update appointment function

--- Backend/way/src/main/java/com/escape/way/controller/AppointmentController.java
+++ Backend/way/src/main/java/com/escape/way/controller/AppointmentController.java
@@ -3,11 +3,9 @@ package com.escape.way.controller;
 import com.escape.way.model.Appointment;
 import com.escape.way.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class AppointmentController {
@@ -17,15 +15,10 @@ public class AppointmentController {
 
     //약속 생성
     @PostMapping(value = "/api/appointment")
-    public Map<String, Object> join(@RequestBody Appointment appointment) {
-        Map<String, Object> response = new HashMap<>();
-
+    public @ResponseBody String join(@RequestBody Appointment appointment) {
         Appointment newAp = appointmentService.createAppointment(appointment);
-
-        if (newAp != null) response.put("result", "SUCCESS");
-        else response.put("result","SUCCESS" );
-
-        return response;
+        if (newAp != null) return "FAIL";
+        else  return "SUCCESS";
     }
 
     // 해당 id를 가진 약속
@@ -36,27 +29,16 @@ public class AppointmentController {
 
     //약속 업데이트
     @PatchMapping("/api/appointment/{no}")
-    public Map<String, Object> updateAppointment(@PathVariable("no") Long no, @RequestBody Appointment appointment) {
-        Map<String, Object> response = new HashMap<>();
-
-        if (appointmentService.updateAppointment(no, appointment) > 0) {
-            response.put("result", "SUCCESS");
-        } else {
-            response.put("result", "SUCCESS");
-        }
-        return response;
+    public ResponseEntity<String> updateAppointment(@PathVariable("no") Long no, @RequestBody Appointment appointment) {
+        if (appointmentService.updateAppointment(no, appointment) > 0) return ResponseEntity.ok("Success");
+        else return ResponseEntity.ok("Fail");
     }
 
     //약속 삭제
     @DeleteMapping("/api/appointment/{no}")
-    public Map<String, Object> deleteAppointment(@PathVariable("no") Long no) {
-        Map<String, Object> response = new HashMap<>();
+    public ResponseEntity<String> deleteAppointment(@PathVariable("no") Long no) {
 
-        if (appointmentService.deleteAppointment(no) > 0) {
-            response.put("result", "SUCCESS");
-        } else {
-            response.put("result", "FAIL");
-        }
-        return response;
+        if (appointmentService.deleteAppointment(no) > 0) return ResponseEntity.ok("Success");
+        else return ResponseEntity.ok("Fail");
     }
 }
